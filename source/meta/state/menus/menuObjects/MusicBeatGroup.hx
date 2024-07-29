92328978502e96fd4db959bd60e1974b5c3b4c46
tchcfhvgd <99186628+tchcfhvgd@users.noreply.github.com>
2024-07-29 08:12:59 +0800

    Update MusicBeatGroup.hx

--- source/meta/state/menus/menuObjects/MusicBeatGroup.hx
+++ source/meta/state/menus/menuObjects/MusicBeatGroup.hx
@@ -4,6 +4,12 @@ import flixel.FlxG;
 import flixel.FlxBasic;
 import flixel.group.FlxGroup.FlxTypedGroup;
 import meta.data.PlayerSettings;
+#if mobile
+import mobile.flixel.FlxVirtualPad;
+import flixel.FlxCamera;
+import flixel.input.actions.FlxActionInput;
+import flixel.util.FlxDestroyUtil;
+#end
 
 /*
 *	just a FlxTypedGroup with step/beat hit functions and controls
@@ -16,6 +22,59 @@ class MusicBeatGroup extends FlxTypedGroup<FlxBasic>
 	
 	inline function get_controls():Controls
 		return PlayerSettings.player1.controls;
+	
+	#if mobile
+		var virtualPad:FlxVirtualPad;
+		var trackedInputsVirtualPad:Array<FlxActionInput> = [];
+
+		public function addVirtualPad(DPad:FlxDPadMode, Action:FlxActionMode)
+		{
+			if (virtualPad != null)
+			removeVirtualPad();
+
+			virtualPad = new FlxVirtualPad(DPad, Action);
+			add(virtualPad);
+
+			controls.setVirtualPadUI(virtualPad, DPad, Action);
+			trackedInputsVirtualPad = controls.trackedInputsUI;
+			controls.trackedInputsUI = [];
+		}
+
+		public function removeVirtualPad()
+		{
+			if (trackedInputsVirtualPad.length > 0)
+			controls.removeVirtualControlsInput(trackedInputsVirtualPad);
+
+			if (virtualPad != null)
+			remove(virtualPad);
+		}
+
+		public function addVirtualPadCamera(DefaultDrawTarget:Bool = true)
+		{
+			if (virtualPad != null)
+			{
+				var camControls:FlxCamera = new FlxCamera();
+				FlxG.cameras.add(camControls, DefaultDrawTarget);
+				camControls.bgColor.alpha = 0;
+				virtualPad.cameras = [camControls];
+			}
+		}
+		#end
+
+		override function destroy()
+		{
+			#if mobile
+			if (trackedInputsVirtualPad.length > 0)
+			controls.removeVirtualControlsInput(trackedInputsVirtualPad);
+			#end
+
+			super.destroy();
+
+			#if mobile
+			if (virtualPad != null)
+			virtualPad = FlxDestroyUtil.destroy(virtualPad);
+			#end
+		}
 	
 	public function new()
 		super();
